662faae64b53c84151c9bfc7b4793eb248ca0f04
César Almeida <cesaralmeida89@gmail.com>
2023-02-09 20:58:14 +0100

    :pencil: add multiple then to promise challenge

--- src/promise/challenge.js
+++ src/promise/challenge.js
@@ -7,11 +7,27 @@ function fetchData(urlApi) {
 	return fetch(urlApi);
 }
 
+// fetchData(`${API}/products`)
+// 	.then(response => response.json())
+// 	.then(products => console.log(products))
+// 	.then(() => {
+// 		console.log('Esto es el tercer then');
+// 	})
+// 	.catch(error => console.log(error))
+// 	.finally(() => console.log('Finally'));
+
 fetchData(`${API}/products`)
 	.then(response => response.json())
-	.then(products => console.log(products))
-	.then(() => {
-		console.log('Esto es el tercer then');
+	.then(products => {
+		console.log(products);
+		return fetchData(`${API}/products/${products[0].id}`);
 	})
-	.catch(error => console.log(error))
-	.finally(() => console.log('Finally'));
+	.then(response => response.json())
+	.then(product => {
+		console.log(product.title);
+		return fetchData(`${API}/categories/${product.category.id}`);
+	})
+	.then(response => response.json())
+	.then(category => console.log(category.name))
+    .catch(error => console.log(error))
+    .finally(() => console.log('Finished'));
